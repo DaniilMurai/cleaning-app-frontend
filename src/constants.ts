42506af848cc1272bce0cdf354f4ef87dfb6f752
DaniilMurai <daniilmuray@gmail.com>
2025-05-13 02:20:52 +0300

    deployed

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,9 @@
 export const DEFAULT_LANG = process.env.EXPO_PUBLIC_DEFAULT_LANG || "en";
-export const ApiUrl = process.env.EXPO_PUBLIC_API_URL || "http://localhost:8000";
+export const ApiUrl =
+	process.env.EXPO_PUBLIC_API_URL || "https://cleaningcompanybackend.onrender.com/";
 
 // Добавьте проверку
-console.log('API URL:', ApiUrl);
+console.log("API URL:", ApiUrl);
 if (!ApiUrl) {
-	console.error('API URL is not defined!');
+	console.error("API URL is not defined!");
 }
